cc8e56e19aab9dab241bca06e0db3dcd17125de4
Anurag Bhardwaj <86941924+theanuraaag@users.noreply.github.com>
2024-02-14 10:17:41 +0530

    setting up the React app

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-red-600 underline text-3xl '>
+        Hello World!
+      </h1>
+    </div>
+  )
+}
